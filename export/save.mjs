81df91bc59b5afee40600d825a0b539a33e72b5f
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-05-24 12:50:48 +0900

    Update save.mjs

--- export/save.mjs
+++ export/save.mjs
@@ -1,6 +1,6 @@
 const _front = location.href.split('?')[0] + '|';
-export const makeSaveKey = key => typeof key === "string" && key.length ? _front + key : '';
+export const makeSaveKey = key => !key ? null : _front + key;
 export const getSaveKeys = () => Object.keys(localStorage).filter(v=>v.includes(_front));
-export const removeSaveData = key => localStorage.removeItem(makeSaveKey(key));
-export const save = (key, val) => localStorage.setItem(makeSaveKey(key), val);
-export const load = key => localStorage.getItem(makeSaveKey(key));
+export const removeSaveData = key => !key ? null : localStorage.removeItem(makeSaveKey(key));
+export const save = (key, val) => !key ? null : localStorage.setItem(makeSaveKey(key), val);
+export const load = key => !key ? null : localStorage.getItem(makeSaveKey(key));
