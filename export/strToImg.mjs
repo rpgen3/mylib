561459de6b52a3e04f4d657713e417529f705fa5
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-28 19:27:19 +0900

    Update strToImg.mjs

--- export/strToImg.mjs
+++ export/strToImg.mjs
@@ -1,50 +1,68 @@
-export const strToImg = str => { // 文字列をbase64のdataURLに変換
-    const ar = [];
-    str.split('').forEach(c=>{
-        const n = c.charCodeAt();
-        if(n < 128) ar.push(n);
-        else {
-            ar.push(128);
-            ar.push((0xff00 & n) >> 8);
-            ar.push((0xff & n));
-        }
-    });
-    const width = Math.ceil(Math.sqrt(ar.length / 3)),
-          cv = document.createElement("canvas");
+export const arrToImg = arr => {
+    const d = new Uint8Array(arr),
+          width = Math.sqrt(d.length / 3) + 1 | 0,
+          area = width ** 2,
+          dd = new Uint8ClampedArray(area * 4);
+    for(let i = 0; i < area; i++){
+        let j = i * 4,
+            k = i * 3;
+        dd[j] = d[k]
+        dd[j + 1] = d[k + 1];
+        dd[j + 2] = d[k + 2];
+        dd[j + 3] = 255;
+    }
+    const cv = document.createElement('canvas');
     cv.width = width;
     cv.height = width;
-    const ctx = cv.getContext("2d"),
-          imgData = ctx.getImageData(0, 0, width, width);
-    let cnt = 0;
-    for(let i = 0; i < ar.length; i++){
-        const i4 = i * 4;
-        for(let o = 0; o < 3; o++) imgData.data[i4 + o] = ar[cnt++] || 0;
-        imgData.data[i4 + 3] = 255; // 透過を指定するとputImageDataで画素値が変わる現象がある
-    }
-    ctx.putImageData(imgData, 0, 0);
-    return cv.toDataURL("image/png");
+    const ctx = cv.getContext('2d');
+    ctx.putImageData(new ImageData(dd, width, width), 0, 0);
+    return cv.toDataURL();
 };
-export const imgToStr = img => { // <img>要素を文字列に変換
-    const { width, height } = img,
-          cv = document.createElement("canvas");
+export const imgToArr = img => {
+    const {width, height} = img,
+          cv = document.createElement('canvas');
     cv.width = width;
     cv.height = height;
     const ctx = cv.getContext('2d');
-    ctx.drawImage(img,0,0);
-    const data = ctx.getImageData(0, 0, width, height).data,
-          ar = [];
-    for(let i = 0; i < data.length; i++){
-        const i4 = i * 4;
-        for(let o = 0; o < 3; o++) ar.push(data[i4 + o]);
+    ctx.drawImage(img, 0, 0);
+    const {data} = ctx.getImageData(0, 0, width, height),
+          area = data.length / 4,
+          d = new Uint8Array(area * 3);
+    for(let i = 0; i < area; i++){
+        let j = i * 3,
+            k = i * 4;
+        d[j] = data[k]
+        d[j + 1] = data[k + 1];
+        d[j + 2] = data[k + 2];
     }
+    return d;
+};
+export const bufToImg = buf => arrToImg(buf);
+export const imgToBuf = img => imgToArr(img).buffer;
+export const imgToAudio = async img => {
+    const ctx = new AudioContext,
+          buf = await ctx.decodeAudioData(imgToBuf(img)),
+          src = ctx.createBufferSource();
+    src.buffer = buf;
+    src.connect(ctx.destination);
+    return src;
+};
+export const strToImg = str => {
+    const arr = [];
+    for(const c of str.split('')){
+        const n = c.charCodeAt();
+        arr.push(n >> 8);
+        arr.push(n & 0xff);
+    }
+    return arrToImg(arr);
+};
+export const imgToStr = img => {
     let str = '';
-    for(let p = 0; p < ar.length; p++){
-        const n = ar[p];
-        if(n < 128) str += String.fromCharCode(n);
-        else if(n === 128){
-            str += String.fromCharCode((ar[p + 1] << 8) + ar[p + 2]);
-            p += 2;
-        }
+    const arr = imgToArr(img),
+          len = arr.length >> 1;
+    for(let i = 0; i < len; i++){
+        const j = i << 1;
+        str += String.fromCharCode((arr[j] << 8) + arr[j + 1]);
     }
     return str.replace(/\0+$/,'');
 };
