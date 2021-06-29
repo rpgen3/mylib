1b45e9cf99423fb6b27fa2d85f579d4419d893a7
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-29 20:08:14 +0900

    Update load.mjs

--- export/load.mjs
+++ export/load.mjs
@@ -1,2 +1,11 @@
 export const importAll = arr => Promise.all(arr.map(v => import(v))).then(v => Object.assign({},...v));
-export const getScript = async url => eval(await(await fetch(url)).text());
+export const getScript = url => new Promise((resolve, reject)=>{
+    const e = document.createElement('script');
+    e.onload = () => {
+        resolve();
+        e.remove();
+    };
+    e.onerror = reject;
+    e.src = url;
+    document.head.append(e);
+});
