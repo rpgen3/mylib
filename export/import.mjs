bcd554d10de86795542ba7d11803915e72d8543e
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-30 18:24:15 +0900

    Update and rename load.mjs to import.mjs

--- export/import.mjs
+++ export/import.mjs
@@ -1,4 +1,5 @@
 export const importAll = arr => Promise.all(arr.map(v => import(v))).then(v => Object.assign({},...v));
+export const importAllSettled = arr => Promise.allSettled(arr.map(v => import(v))).then(v => Object.assign({},...v.flatMap(({value}) => value ? value : {})));
 export const getScript = url => new Promise((resolve, reject)=>{
     const e = document.createElement('script');
     e.onload = () => {
