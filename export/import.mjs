22cec7e7c11edc2fb6d805584c885e6f90d2b762
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-13 09:57:46 +0900

    Update import.mjs

--- export/import.mjs
+++ export/import.mjs
@@ -2,19 +2,19 @@ export const importAll = arr => Promise.all(arr.map(v => import(v))).then(v => O
 export const importAllSettled = arr => Promise.allSettled(arr.map(v => import(v))).then(v => Object.assign({},...v.flatMap(({value}) => value ? value : {})));
 export const getScript = url => new Promise((resolve, reject)=>{
     const e = document.createElement('script');
+    document.head.append(e);
     e.onload = () => {
         resolve();
         e.remove();
     };
     e.onerror = reject;
     e.src = url;
-    document.head.append(e);
 });
 export const getCSS = url => new Promise((resolve, reject)=>{
     const e = document.createElement('link');
+    document.head.append(e);
     e.rel = 'stylesheet';
     e.onload = resolve;
     e.onerror = reject;
     e.href = url;
-    document.head.append(e);
 });
