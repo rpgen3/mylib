727eeda1d9ae496fa54a3b4ab1dd4e2ce1d078d7
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-11-20 12:40:49 +0900

    Update util.mjs

--- export/util.mjs
+++ export/util.mjs
@@ -1,15 +1,14 @@
 export const isIterable = x => Symbol.iterator in x; // イテラブルか判定
 export const getType = x => Object.prototype.toString.call(x).replace(/\[object |\]/g,''); // 型名を返す
 export const getTime = date => { // xx:yy:zz の形式で現在時刻の文字列を返す
-    let d = date ? new Date(date) : new Date;
-    if(isNaN(d)) throw new Error("argument 1 needs to be a date");
+    let d = date ? new Date(date) : Date.now();
+    if(Number.isNaN(Number(d))) throw new Error("argument 1 needs to be a date");
     d /= 1000;
     const s = d % 60;
     d /= 60;
     const m = d % 60;
     d /= 60;
-    const h = (d + 9) % 24; // 東京都のタイムゾーン
-    return [h,m,s].map(v=>('00'+Math.floor(v)).slice(-2)).join(':');
+    return [d, m, s].map(v=>('0' + v | 0).slice(-2)).join(':');
 };
 export const copy = str => { // 文字列をクリップボードにコピー
     const e = document.createElement("textarea");
