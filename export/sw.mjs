2e0155de9598872d9afd12b4d748ab534939f466
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-11-28 13:51:30 +0900

    Update sw.mjs

--- export/sw.mjs
+++ export/sw.mjs
@@ -1,8 +1,5 @@
 const def = 'default';
 export const sw = x => p => {
-    for(const k in p) {
-        if(k === def) continue;
-        else if(x == k) return p[k](x);
-    }
+    for(const k in p) if(k !== def && x == k) return p[k](x);
     return p.default(x);
 };
